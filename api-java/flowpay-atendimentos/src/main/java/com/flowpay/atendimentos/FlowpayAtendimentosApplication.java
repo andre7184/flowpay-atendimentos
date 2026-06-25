bdfadb5223285ab8de4a2ae3ce001aaa3c4bc521
andre7184 <amb7184@gmail.com>
2026-06-24 22:55:37 -0400

    Create FlowpayAtendimentosApplication.java

---
+++ api-java/flowpay-atendimentos/src/main/java/com/flowpay/atendimentos/FlowpayAtendimentosApplication.java
@@ -0,0 +1,12 @@
+package com.flowpay.atendimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowpayAtendimentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowpayAtendimentosApplication.class, args);
+    }
+}
